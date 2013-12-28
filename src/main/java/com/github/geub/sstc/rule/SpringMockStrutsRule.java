79ab06fa1f02e60aada5831ce74a7f634b383cf0
hm1rafael <hm1rafael@gmail.com>
2013-12-28 14:02:58 -0200

    Update logic

---
+++ src/main/java/com/github/geub/sstc/rule/SpringMockStrutsRule.java
@@ -0,0 +1,35 @@
+package com.github.geub.sstc.rule;
+
+import org.apache.struts.config.ModuleConfigFactory;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+
+import com.github.geub.sstc.annotations.StrutsAction;
+import com.github.geub.sstc.mock.SpringMockStrutsTestCase;
+
+public class SpringMockStrutsRule extends TestWatcher {
+
+	private SpringMockStrutsTestCase springMockStrutsTestCase;
+	
+	@Override
+	protected void starting(Description description) {
+		try {
+			ModuleConfigFactory.setFactoryClass(factoryClass);
+			StrutsAction strutsAction = description.getAnnotation(StrutsAction.class);
+			this.springMockStrutsTestCase = new SpringMockStrutsTestCase(strutsAction);
+			this.springMockStrutsTestCase.configureTest();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	@Override
+	protected void succeeded(Description description) {
+		super.succeeded(description);
+	}
+
+	public void actionPerform() {
+		springMockStrutsTestCase.actionPerform();
+	}
+	
+}
