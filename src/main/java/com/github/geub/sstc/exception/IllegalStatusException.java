547605659496b44a6b899be85ffa2903c246df7e
hm1rafael <hm1rafael@gmail.com>
2014-01-08 11:20:08 -0200

    execption to demonstrate the illegal status

---
+++ src/main/java/com/github/geub/sstc/exception/IllegalStatusException.java
@@ -0,0 +1,9 @@
+package com.github.geub.sstc.exception;
+
+public class IllegalStatusException extends IllegalStateException {
+
+	public IllegalStatusException(String msg) {
+		super(msg);
+	}
+
+}
